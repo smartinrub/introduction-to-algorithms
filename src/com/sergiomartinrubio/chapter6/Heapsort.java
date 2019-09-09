ed8a1de8809265737eb10f71cefad04623a01189
Sergio Martin Rubio <econsergio@gmail.com>
2019-09-09 20:00:35 +0100

    fix heapsort algorithm

--- src/com/sergiomartinrubio/chapter6/Heapsort.java
+++ src/com/sergiomartinrubio/chapter6/Heapsort.java
@@ -1,41 +1,39 @@
 package com.sergiomartinrubio.chapter6;
 
-import java.util.Arrays;
-
 public class Heapsort {
 
     public void sort(int[] A) {
         buildMaxHeap(A);
-        for (int i = A.length - 1; i > 1; i--) {
-            int temp = A[1];
-            A[1] = A[i];
+        for (int i = A.length - 1; i > 0; i--) {
+            int temp = A[0];
+            A[0] = A[i];
             A[i] = temp;
-            A = Arrays.copyOfRange(A, 0, A.length - 1);
-            maxHeap(A, 0);
+            maxHeap(A, i,0);
         }
     }
 
     private void buildMaxHeap(int[] A) {
-        for (int i = A.length / 2 - 1; i >= 0; i--) {
-            maxHeap(A, i);
+        int n = A.length;
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            maxHeap(A, n, i);
         }
     }
 
-    private void maxHeap(int[] A, int i) {
+    private void maxHeap(int[] A, int n, int i) {
         int l = i * 2 + 1;
         int r = i * 2 + 2;
         int largest = i;
-        if (l < A.length && A[l] > A[i]) {
+        if (l < n && A[l] > A[i]) {
             largest = l;
         }
-        if (r < A.length && A[r] > A[largest]) {
+        if (r < n && A[r] > A[largest]) {
             largest = r;
         }
         if (largest != i) {
             int temp = A[i];
             A[i] = A[largest];
             A[largest] = temp;
-            maxHeap(A, largest);
+            maxHeap(A, n, largest);
         }
     }
 }
