b767c8ba352690c1d3a06b1c21a315569a162ebf
Sergio Martin Rubio <econsergio@gmail.com>
2019-09-17 23:34:32 +0100

    remove commented lines

--- src/com/sergiomartinrubio/chapter10/Main.java
+++ src/com/sergiomartinrubio/chapter10/Main.java
@@ -3,45 +3,45 @@ package com.sergiomartinrubio.chapter10;
 public class Main {
 
     public static void main(String[] args) {
-//        Stack<Integer> stack = new Stack<>();
-//        stack.push(3);
-//        stack.push(5);
-//        stack.push(9);
-//        stack.push(1);
-//
-//        System.out.println(stack.pop());
-//        System.out.println(stack.pop());
-//        System.out.println(stack.pop());
-//        System.out.println(stack.pop());
-//
-//        Queue<Integer> queue = new Queue<>();
-//        queue.enqueue(3);
-//        queue.enqueue(5);
-//        queue.enqueue(9);
-//        queue.enqueue(1);
-//
-//        System.out.println(queue.dequeue());
-//        System.out.println(queue.dequeue());
-//        System.out.println(queue.dequeue());
-//        System.out.println(queue.dequeue());
-//
-//        Deque<Integer> deque = new Deque<>();
-//        deque.insertFront(3);
-//        deque.insertFront(5);
-//        deque.insertFront(9);
-//        deque.insertFront(1);
-//        deque.insertLast(8);
-//
-//        System.out.println(deque.getFront());
-//        System.out.println(deque.getRear());
-//
-//        deque.deleteFront();
-//        deque.deleteRear();
-//        System.out.println(deque.getFront());
-//        System.out.println(deque.getRear());
-//
-//        deque.deleteRear();
-//        System.out.println(deque.getRear());
+        Stack<Integer> stack = new Stack<>();
+        stack.push(3);
+        stack.push(5);
+        stack.push(9);
+        stack.push(1);
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+
+        Queue<Integer> queue = new Queue<>();
+        queue.enqueue(3);
+        queue.enqueue(5);
+        queue.enqueue(9);
+        queue.enqueue(1);
+
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+
+        Deque<Integer> deque = new Deque<>();
+        deque.insertFront(3);
+        deque.insertFront(5);
+        deque.insertFront(9);
+        deque.insertFront(1);
+        deque.insertLast(8);
+
+        System.out.println(deque.getFront());
+        System.out.println(deque.getRear());
+
+        deque.deleteFront();
+        deque.deleteRear();
+        System.out.println(deque.getFront());
+        System.out.println(deque.getRear());
+
+        deque.deleteRear();
+        System.out.println(deque.getRear());
 
 
         DoubleLinkedList<Integer> doubleLinkedList = new DoubleLinkedList<>();
